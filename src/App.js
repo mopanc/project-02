ac2d634cf0f38491c34ae092a2ae6c478d980201
mopanc <jorgemopanc@icloud.com>
2021-08-14 21:29:31 -0300

    Create react  app and test first use state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  //const { reverse } = this.state;
+  const [reverse, setReverse] = useState(false);
+  const [counter, setCounter] = useState(0);
+  const reverseClass = reverse ? 'reverse' : '';
+
+  const handleClick = () => {
+    //this.setState({ reverse: !reverse });
+    setReverse(!reverse);
+  };
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+        <h1>Likes: {counter}</h1>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button type="button" className="reverseClass" onClick={handleClick}>
+            Reverse
+          </button>{' '}
+          <button type="button" className="reverseClass" onClick={handleIncrement}>
+            like
+          </button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
 
+//class App extends Component {
+//  state = {
+//    reverse: false,
+//  };
+//
+//  handleClick = () => {
+//    const { reverse } = this.state;
+//    this.setState({ reverse: !reverse });
+//  };
+//
+//  render() {
+//    const { reverse } = this.state;
+//    const reverseClass = reverse ? 'reverse' : '';
+//
+//    return (
+//      <div className="App">
+//        <header className="App-header">
+//          <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+//          <button type="button" className="reverseClass" onClick={this.handleClick}>
+//            Reverse
+//          </button>
+//        </header>
+//      </div>
+//    );
+//  }
+//}
+//
 export default App;
